27c154c64f3cf4237311ff35eb57c102b0e435e8
Ammar Zafar Raja <ammar.raja7393@users.noreply.github.com>
2017-09-18 11:32:13 +1000

    Carpark Even Method
    Testing Carpark event Method

--- test/bcccp/carpark/entry/EntryControllerTest.java
+++ test/bcccp/carpark/entry/EntryControllerTest.java
@@ -84,7 +84,7 @@ public class EntryControllerTest {
         EntryController instance = null;
         instance.ticketTaken();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+      
     }
 
     /**
