f2b58d9eab264990e3c0fb92eef2a29d31a680aa
Ammar Zafar Raja <ammar.raja7393@users.noreply.github.com>
2017-09-18 11:43:24 +1000

    Pushbutton class
    Creating stub method in Pushbutton class

--- test/bcccp/carpark/entry/IEntryControllerTest.java
+++ test/bcccp/carpark/entry/IEntryControllerTest.java
@@ -44,7 +44,7 @@ public class IEntryControllerTest {
         IEntryController instance = new IEntryControllerImpl();
         instance.buttonPushed();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
