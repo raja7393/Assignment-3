7a9530ee1990f7b19d753bebba7d511dbdc0cb27
Ammar Zafar Raja <ammar.raja7393@users.noreply.github.com>
2017-09-18 11:32:50 +1000

    Pushbutton class
    Testing Pushbutton Module

--- test/bcccp/carpark/entry/EntryControllerTest.java
+++ test/bcccp/carpark/entry/EntryControllerTest.java
@@ -59,7 +59,7 @@ public class EntryControllerTest {
         EntryController instance = null;
         instance.buttonPushed();
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
