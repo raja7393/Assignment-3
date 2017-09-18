85d246c772a2e7f165c540a445ec522d24429cd8
Ammar Zafar Raja <ammar.raja7393@users.noreply.github.com>
2017-09-18 11:31:15 +1000

    Entry Controller Module Junit Testing
    Testing done thorugh specifying parameters.

--- test/bcccp/carpark/entry/EntryControllerTest.java
+++ test/bcccp/carpark/entry/EntryControllerTest.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ 11618519- Ammar Zafar Raja
+ Junit Testing of Entry Controller module.
  */
 package bcccp.carpark.entry;
 
@@ -14,7 +13,7 @@ import static org.junit.Assert.*;
 
 /**
  *
- * @author 11618519
+ * @author 11618519- Ammar Zafar Raja
  */
 public class EntryControllerTest {
     
