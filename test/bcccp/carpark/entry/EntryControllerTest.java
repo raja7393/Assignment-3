e84cf7035b7966c208147b954391f0ac58449de8
Ammar Zafar Raja <ammar.raja7393@users.noreply.github.com>
2017-09-18 11:33:39 +1000

    Careventdetected
    Testing whether the system detects the car or not.

--- test/bcccp/carpark/entry/EntryControllerTest.java
+++ test/bcccp/carpark/entry/EntryControllerTest.java
@@ -47,7 +47,7 @@ public class EntryControllerTest {
         EntryController instance = null;
         instance.carEventDetected(detectorId, carDetected);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
