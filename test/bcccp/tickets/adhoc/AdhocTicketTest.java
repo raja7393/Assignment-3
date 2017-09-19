b69b50d31fe3349d5346e5f00d315d3697d8e84f
Ammar11618519 <11618519+raja7393@users.noreply.github.com>
2017-09-19 23:40:08 +1000

    Test of getBarcode method
    The test case is a prototype.

--- test/bcccp/tickets/adhoc/AdhocTicketTest.java
+++ test/bcccp/tickets/adhoc/AdhocTicketTest.java
@@ -44,7 +44,7 @@ public class AdhocTicketTest {
         String result = instance.getBarcode();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
