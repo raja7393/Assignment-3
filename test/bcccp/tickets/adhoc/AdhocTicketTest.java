16cc827c6a5048dd128d7704af5417aea4fb2ffa
Ammar11618519 <11618519+raja7393@users.noreply.github.com>
2017-09-19 23:49:56 +1000

    testGetPaidDateTime
    The test case is a prototype

--- test/bcccp/tickets/adhoc/AdhocTicketTest.java
+++ test/bcccp/tickets/adhoc/AdhocTicketTest.java
@@ -126,7 +126,7 @@ public class AdhocTicketTest {
         long result = instance.getPaidDateTime();
         assertEquals(expResult, result);
         // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        
     }
 
     /**
